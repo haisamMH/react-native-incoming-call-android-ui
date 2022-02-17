62e9b7205d11e9f3c5ed836c5324f0de07004a07
Haisam Mohamed Hashim <hyshamhashim@gmail.com>
2022-02-17 22:11:22 +0000

    first commit

---
+++ android/src/main/java/com/incomingcall/UnlockScreenActivity.java
@@ -0,0 +1,203 @@
+package com.incomingcall;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.view.View;
+import android.net.Uri;
+import android.os.Vibrator;
+import android.content.Context;
+import android.media.MediaPlayer;
+import android.provider.Settings;
+import java.util.List;
+import android.app.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.app.ActivityManager;
+import android.app.ActivityManager.RunningAppProcessInfo;
+
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.ReactContext;
+import com.facebook.react.bridge.ReadableMap;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.modules.core.DeviceEventManagerModule;
+
+import com.squareup.picasso.Picasso;
+
+public class UnlockScreenActivity extends AppCompatActivity implements UnlockScreenActivityInterface {
+
+    private static final String TAG = "MessagingService";
+    private TextView tvName;
+    private TextView tvInfo;
+    private ImageView ivAvatar;
+    private String uuid = "";
+    static boolean active = false;
+    private static Vibrator v = (Vibrator) IncomingCallModule.reactContext.getSystemService(Context.VIBRATOR_SERVICE);
+    private long[] pattern = { 0, 1000, 800 };
+    private static MediaPlayer player = MediaPlayer.create(IncomingCallModule.reactContext,
+            Settings.System.DEFAULT_RINGTONE_URI);
+    private static Activity fa;
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        active = true;
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        active = false;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        fa = this;
+
+        setContentView(R.layout.activity_call_incoming);
+
+        tvName = findViewById(R.id.tvName);
+        tvInfo = findViewById(R.id.tvInfo);
+        ivAvatar = findViewById(R.id.ivAvatar);
+
+        Bundle bundle = getIntent().getExtras();
+        if (bundle != null) {
+            if (bundle.containsKey("uuid")) {
+                uuid = bundle.getString("uuid");
+            }
+            if (bundle.containsKey("name")) {
+                String name = bundle.getString("name");
+                tvName.setText(name);
+            }
+            if (bundle.containsKey("info")) {
+                String info = bundle.getString("info");
+                tvInfo.setText(info);
+            }
+            if (bundle.containsKey("avatar")) {
+                String avatar = bundle.getString("avatar");
+                if (avatar != null) {
+                    Picasso.get().load(avatar).transform(new CircleTransform()).into(ivAvatar);
+                }
+            }
+        }
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN | WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON
+                | WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
+                | WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED | WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD);
+
+        // v.vibrate(pattern, 0);
+        player.start();
+
+        AnimateImage acceptCallBtn = findViewById(R.id.ivAcceptCall);
+        acceptCallBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    // v.cancel();
+                    player.stop();
+                    acceptDialing();
+                } catch (Exception e) {
+                    WritableMap params = Arguments.createMap();
+                    params.putString("message", e.getMessage());
+                    sendEvent("error", params);
+                    dismissDialing();
+                }
+            }
+        });
+
+        AnimateImage rejectCallBtn = findViewById(R.id.ivDeclineCall);
+        rejectCallBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // v.cancel();
+                player.stop();
+                dismissDialing();
+            }
+        });
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        // Dont back
+    }
+
+    public static void dismissIncoming() {
+        try{
+            // v.cancel();
+            player.stop();
+            fa.finish();
+        }catch(Exception e){
+            // WritableMap params = Arguments.createMap();
+            // params.putString("message", e.getMessage());
+            // sendEvent("error", params);
+            // dismissDialing();
+        }
+    }
+
+    private void acceptDialing() {
+        WritableMap params = Arguments.createMap();
+        params.putBoolean("accept", true);
+        params.putString("uuid", uuid);
+        if (!IncomingCallModule.reactContext.hasCurrentActivity()) {
+            params.putBoolean("isHeadless", true);
+        }
+
+        sendEvent("answerCall", params);
+
+        finish();
+    }
+
+    private void dismissDialing() {
+        WritableMap params = Arguments.createMap();
+        params.putBoolean("accept", false);
+        params.putString("uuid", uuid);
+        if (!IncomingCallModule.reactContext.hasCurrentActivity()) {
+            params.putBoolean("isHeadless", true);
+        }
+
+        sendEvent("endCall", params);
+
+        finish();
+    }
+
+    @Override
+    public void onConnected() {
+        Log.d(TAG, "onConnected: ");
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+
+            }
+        });
+    }
+
+    @Override
+    public void onDisconnected() {
+        Log.d(TAG, "onDisconnected: ");
+
+    }
+
+    @Override
+    public void onConnectFailure() {
+        Log.d(TAG, "onConnectFailure: ");
+
+    }
+
+    @Override
+    public void onIncoming(ReadableMap params) {
+        Log.d(TAG, "onIncoming: ");
+    }
+
+    private void sendEvent(String eventName, WritableMap params) {
+        IncomingCallModule.reactContext
+                .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+                .emit(eventName, params);
+    }
+}
